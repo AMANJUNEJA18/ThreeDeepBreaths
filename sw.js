9cd2cf4405bb014209671fb2c251eeaba313e5ac
Kabir <AMANJUNEJA18@GMAIL.COM>
2026-08-14 19:38:06 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'tdb-final-20260814-night-v3';
+const CACHE_VERSION = 'tdb-final-20260814-night-v4';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const STATIC_ASSETS = [
   '/manifest.json',
