fc485d5abff9f57946269dc6ec25694b6c155678
Kabir <AMANJUNEJA18@GMAIL.COM>
2026-08-23 10:36:44 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'tdb-final-20260823-be-v11';
+const CACHE_VERSION = 'tdb-final-20260823-be-v12';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const STATIC_ASSETS = [
   '/index.html',
